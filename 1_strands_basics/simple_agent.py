09673578c87b02b3663f05ea0d87ee6985687e52
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2026-02-08 21:59:14 +0200

    1_strands_basics/simple_agent.py

--- 1_strands_basics/simple_agent.py
+++ 1_strands_basics/simple_agent.py
@@ -1,7 +1,22 @@
 from strands import Agent
+import logging
 
-# TODO: Add debug logging to see what your agent is thinking
 
-# TODO: Create the agent with the following system prompt: "You are a game master for a Dungeon & Dragon game"
 
-# TODO: Summon your agent with a basic incantation such as "Hi, I am an advanturer ready for adventure!"
+# Configure the root strands logger
+logging.getLogger("strands").setLevel(logging.DEBUG)
+
+# Add a handler to see the logs
+logging.basicConfig(
+    format="%(levelname)s | %(name)s | %(message)s",
+    handlers=[logging.StreamHandler()]
+)
+
+
+agent = Agent(
+    system_prompt=(
+        "You are a game master for a Dungeon & Dragon game")
+)
+
+
+agent("Hi, I am an advanturer ready for adventure!")
